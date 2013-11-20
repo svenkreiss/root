a2bcdca18c465fb3585021c53fc0f5c9b2788d80
Sven Kreiss <sk@svenkreiss.com>
2013-11-20 15:34:34 -0500

    Put PiecewiseInterpolation limit back in (it's wrong, but without it the fit seems unstable).

--- roofit/histfactory/src/PiecewiseInterpolation.cxx
+++ roofit/histfactory/src/PiecewiseInterpolation.cxx
@@ -234,7 +234,7 @@ Double_t PiecewiseInterpolation::evaluate() const
 	double xx = x*x ;
 	double xxxx = xx*xx ;
 	double val = nominal + S*x + A*(15*xx - 10*xxxx + 3*xxxx*xx);
-	//if (val < 0) val = 0;
+	if (val < 0) val = 0;
 	sum += val-nominal;
       }
       break ;
